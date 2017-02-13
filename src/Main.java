1c1b40501691ae950c951bccd7d6f3d41fdc1dbe
Rob Culclasure <robculclasure@Robs-MacBook-Pro.local>
2017-02-12 18:23:58 -0600

    Rewrote file scanning method to load a hashmap instead of an arraylist.

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,40 @@ import java.util.Scanner;
 
 public class Main {
 
-    static HashMap<String, ArrayList<Country>> hashMap = new HashMap();
-    static ArrayList<Country> listOfAllCountries = new ArrayList<>();
+    static HashMap<String, ArrayList<Country>> Atlas = new HashMap();
+    static ArrayList<Country> listOfCountries = new ArrayList<>();
 
-    public static void scanFileIntoArray() throws FileNotFoundException {
+    public static void scanFileIntoHashMap() throws FileNotFoundException {
         File f = new File("countries.txt");
         Scanner fileScanner = new Scanner(f);
 
+        String firstLetter = "a";
+        String lastFirstLetter;
+
         while(fileScanner.hasNext()){
+            lastFirstLetter = firstLetter;
             String line = fileScanner.nextLine();
             String[] columns = line.split("\\|");
-            Country country = new Country(columns[0], columns[1]);
-            listOfAllCountries.add(country);
+
+            firstLetter = columns[1].substring(0,1);
+
+            if(firstLetter.equalsIgnoreCase(lastFirstLetter)){
+                Country country = new Country(columns[0], columns[1]);
+                listOfCountries.add(country);
+            }
+            else{
+                Atlas.put(lastFirstLetter, listOfCountries);
+                listOfCountries.clear();
+                Country country = new Country(columns[0], columns[1]);
+                listOfCountries.add(country);
+            }
         }
         fileScanner.close();
     }
 
     public static void main(String[] args) throws Exception {
 
-        scanFileIntoArray();
+        scanFileIntoHashMap();
 
         System.out.println("Please enter a single letter:");
         Scanner letterScanner = new Scanner(System.in);
@@ -38,12 +53,12 @@ public class Main {
             throw new Exception("Single letter error.");
         }
         else if(userEntered.length() == 1){
-            for (Country country : listOfAllCountries){
+            for (Country country : listOfCountries){
                 if(userEntered.equalsIgnoreCase(country.getName().substring(0,1))){
                     listOfXCountries.add(country);
                 }
             }
-            hashMap.put(userEntered, listOfXCountries);
+            Atlas.put(userEntered, listOfXCountries);
 
             File g = new File(userEntered + "_countries.txt");
             FileWriter fw = new FileWriter(g);
